a4f67a7bd46794bbb44a21fbf61970684719bca0
ataricco <ataricco@wpi.edu>
2022-03-22 20:18:24 -0400

    Added Comment

--- app/src/main/java/com/example/angtestapp/MainActivity.java
+++ app/src/main/java/com/example/angtestapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // ANG WAS HERE
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
